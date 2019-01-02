adbfbe0f19d6bf9f8d2475995562e4f623244bae
sgalicia <44712840+sgalicia@users.noreply.github.com>
2019-01-02 12:22:29 -0500

    Add files via upload

---
+++ Lesson 9/problem2.py
@@ -0,0 +1,13 @@
+
+print('-'*60)
+print('Locker Bot: ')
+print()
+password = input('What is the password? ')
+if password == 'Open Sesame':
+	
+	print('Password is valid')
+	print('Locker Opening')
+else:
+	print('Password is invalid..enabling laser defesnse system, please stand down.')
+	print('Pew Pew Pew!')
+print('-'*60)
---
+++ Lesson 9/problem3.py
@@ -0,0 +1,9 @@
+print('-'*60)
+print('Alien Greeter Bot: ')
+print()
+planet = input('What planet are you from? ')
+if planet == 'Mars':
+	print('Welcome to Earth, please do not kill us!')
+else:
+	print('Dammmmnnnnn, your face is so ugly I thought you were an alien. Anyways ... have a good day!')
+print('-'*60)
